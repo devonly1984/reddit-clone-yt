f73624226b365f805468c41f82ed1fb744bf8729
John NoBody <devonly1984@hotmail.com>
2022-05-22 07:08:28 -0500

    Added next-auth
    Signed-off-by: John NoBody <devonly1984@hotmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,22 @@
 import Image from 'next/image'
-import { ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+  MenuIcon,
+} from '@heroicons/react/solid'
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/outline'
 const Header = () => {
   return (
-    <div className="flex bg-white px-4 py-2 shadow-sm">
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Image
           objectFit="contain"
@@ -10,10 +24,46 @@ const Header = () => {
           layout="fill"
         />
       </div>
-      <div className="mx-7 flex items-center">
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
-        <p>Home</p>
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
         <ChevronDownIcon className="h5 w-5" />
+      </div>
+      {/**Search
+       *
+       */}
+      <form className=" flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button hidden type="submit" />
+      </form>
+      <div className="mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className=" h-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center lg:hidden">
+        <MenuIcon className="icon" />
+      </div>
+      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
+        <div className="relative h-5 w-5 flex-shrink-0">
+          <Image
+            objectFit="contain"
+            src="https://links.papareact.com/23l"
+            alt=""
+            layout="fill"
+          />
+        </div>
+        <p className="text-gray-400">Sign In</p>
       </div>
     </div>
   )
--- pages/api/auth/[...next-auth].js
+++
@@ -1,5 +0,0 @@
-import NextAuth from 'next-auth'
-import { RedditProvider } from 'next-auth/providers/reddit'
-export default NextAuth({
-  providers: [],
-})
